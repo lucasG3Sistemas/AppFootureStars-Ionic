7f43add9723b7be47bca0466b9dd99fa48775dc1
lucas@G3Sistemas <lucas@g3sistemas.com>
2019-10-09 22:39:21 -0300

    Criação da Página de Feed para os Jogadores

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,37 +5,33 @@ import { MyApp } from './app.component';
 
 import { AboutPage } from '../pages/about/about';
 import { ContactPage } from '../pages/contact/contact';
-import { HomePage } from '../pages/home/home';
+import { FeedPage } from '../pages/feed/feed';
 import { TabsPage } from '../pages/tabs/tabs';
 
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
 import { IntroPageModule } from '../pages/intro/intro.module';
-//import { FeedPage } from '../pages/feed/feed';
-import { FeedPageModule } from '../pages/feed/feed.module';
 import { ConfigProvider } from '../providers/config/config';
 
 @NgModule({
   declarations: [
     MyApp,
-  //  FeedPage,
     AboutPage,
     ContactPage,
-    HomePage,
+    FeedPage,
     TabsPage
   ],
   imports: [
     BrowserModule,
     IonicModule.forRoot(MyApp),
     IntroPageModule,
-    FeedPageModule
   ],
   bootstrap: [IonicApp],
   entryComponents: [
     MyApp,
     AboutPage,
     ContactPage,
-    HomePage,
+    FeedPage,
     TabsPage
   ],
   providers: [
