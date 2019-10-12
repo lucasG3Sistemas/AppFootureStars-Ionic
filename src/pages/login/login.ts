51d17fd20531a1212481008424039ad022b8c6b6
lucas@G3Sistemas <lucas@g3sistemas.com>
2019-10-12 12:54:06 -0300

    Navegação da Tela de Login para a HomePage

--- src/pages/login/login.ts
+++ src/pages/login/login.ts
@@ -1,12 +1,7 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { TabsPage } from '../tabs/tabs';
 
-/**
- * Generated class for the LoginPage page.
- *
- * See https://ionicframework.com/docs/components/#navigation for more info on
- * Ionic pages and navigation.
- */
 
 @IonicPage()
 @Component({
@@ -20,6 +15,10 @@ export class LoginPage {
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad LoginPage');
+  }
+
+  login() {
+    this.navCtrl.push(TabsPage);
   }
 
 }
